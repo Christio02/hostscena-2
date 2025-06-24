8af2dc9113f1627623d9872e3857684c8c04ab16
Christopher Høe <christopher.hoee@gmail.com>
2025-06-24 22:21:59 +0200

    tailwind init upgrade

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,30 +1,34 @@
-import typography from '@tailwindcss/typography'
-import type { Config } from 'tailwindcss'
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       screens: {
-        mobile: '450px',
-        phone: '720px',
-        tablet: '990px',
-        laptop: '1150px',
-        desktop: '1440px',
+        mobile: "450px",
+        phone: "720px",
+        tablet: "990px",
+        laptop: "1150px",
+        desktop: "1440px",
       },
       fontFamily: {
-        sans: ['var(--font-source)'],
-        heading: ['var(--font-wittgenstein)'],
+        sans: ["var(--font-source)"],
+        heading: ["var(--font-wittgenstein)"],
       },
       colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      fontSize: {
+        // custom body font size utility
+        body: "var(--body)",
       },
     },
   },
   plugins: [typography],
-} satisfies Config
+} satisfies Config;
