d5fd55eaf8ae78486deea48f64e2e53a68ca048b
August Solli Middelkoop <august.kampen@gmail.com>
2025-06-25 10:52:00 +0200

    DisableDraftMode.tsx fix

--- frontend/components/sanitylive/DisableDraftMode.tsx
+++ frontend/components/sanitylive/DisableDraftMode.tsx
@@ -1,32 +1,39 @@
 "use client";
 
-import { useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { disableDraftMode } from "@/app/actions";
 
 export function DisableDraftMode() {
   const router = useRouter();
   const [pending, startTransition] = useTransition();
+  const [isEmbedded, setIsEmbedded] = useState(false);
 
-  if (window !== window.parent || !!window.opener) {
+  useEffect(() => {
+    if (window !== window.parent || !!window.opener) {
+      setIsEmbedded(true);
+    }
+  }, []);
+
+  if (isEmbedded) {
     return null;
   }
 
   const disable = () =>
-    startTransition(async () => {
-      await disableDraftMode();
-      router.refresh();
-    });
+      startTransition(async () => {
+        await disableDraftMode();
+        router.refresh();
+      });
 
   return (
-    <div>
-      {pending ? (
-        "Disabling draft mode..."
-      ) : (
-        <button type="button" onClick={disable}>
-          Disable draft mode
-        </button>
-      )}
-    </div>
+      <div>
+        {pending ? (
+            "Disabling draft mode..."
+        ) : (
+            <button type="button" onClick={disable}>
+              Disable draft mode
+            </button>
+        )}
+      </div>
   );
 }
